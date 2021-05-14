25bfd8f159279624b489271af19971603a0e83f6
Elias <elias.saavedra.dev@gmail.com>
2021-05-14 10:11:13 -0300

    axiosJson

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
     "Access-Control-Allow-Methods": "*"
   }
 });
-axios.get('https://api.instagram.com/oauth/authorize?client_id={553855078936098}&redirect_uri={https://elias-dev.github.io/igpp/}&scope=user_profile,user_media&response_type=code')
+axios.get('https://api.instagram.com/oauth/authorize?client_id=553855078936098&redirect_uri=https://elias-dev.github.io/igpp/&scope=user_profile,user_media&response_type=code')
   .then(function (response) {
     // handle success
     console.log(response);
